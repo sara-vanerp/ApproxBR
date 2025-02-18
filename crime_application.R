068baf9216fb1feb0e19c88784b3a77811eff6cc
sara-vanerp <sara.vanerp@gmail.com>
2025-02-18 15:02:04 +0100

    update revised crime application

--- crime_application.R
+++ crime_application.R
@@ -333,6 +333,56 @@ df.sel %>%
 
 
 ##### Analyses revision 1 -----
+## Visualize the results on a diagonal (in line with other figures)
+load("./results/full_results_crime.RData")
+
+approx <- res[which(res$Algorithm == "shrinkem"), ]
+colnames(approx) <- c("Variable", "Prior", "Algorithm", 
+                      "Approx. mean", "Approx. LB", "Approx. UB",
+                      "Approx. mode", "Approx. incl.")
+exact <- res[which(res$Algorithm == "exact"), ]
+colnames(exact) <- c("Variable", "Prior", "Algorithm",
+                     "Exact mean", "Exact LB", "Exact UB",
+                     "Exact mode", "Exact incl.")
+
+plotdat <- merge(approx, exact, by=c("Variable", "Prior"))
+plotdat$Prior <- plyr::revalue(plotdat$Prior, 
+                             c("hs" = "Horseshoe",
+                               "lasso" = "Lasso",
+                               "ridge" = "Ridge"))
+
+png(file = "./results/points_intervals_diagonal_crime.png", width = 1400, height = 800)
+ggplot(plotdat, aes(x = `Exact mean`, y = `Approx. mean`)) +
+  facet_wrap("Prior") +
+  geom_point() +
+  coord_cartesian(xlim = c(-1, 1), ylim = c(-1, 1)) +
+  geom_abline(intercept = 0, slope = 1, lty = 2) +
+  geom_linerange(aes(ymin = `Approx. LB`, ymax = `Approx. UB`)) +
+  geom_linerange(aes(xmin = `Exact LB`, xmax = `Exact UB`)) +
+  xlab("Exact") + ylab("Approximate") +
+  theme_bw(base_size = 25)
+dev.off()
+
+## Visualize histograms of the modes
+load("./results/full_results_crime.RData")
+
+res <- res[which(res$Algorithm != "approx"), ] # remove approximate implementation stan
+
+res$Prior <- plyr::revalue(res$Prior, 
+                               c("hs" = "Horseshoe",
+                                 "lasso" = "Lasso",
+                                 "ridge" = "Ridge"))
+
+png(file = "./results/hist_mode_crime.png", width = 1000, height = 800)
+ggplot(res, aes(x = `Mode`)) +
+  geom_histogram() +
+  facet_grid(`Algorithm`~`Prior`) +
+  theme_bw(base_size = 25)
+dev.off()
+
+##### Additional cross-validation on the full data and half of the data -----
+## Note: 25% of the data is not possible because then ML estimates are no longer available
+
 ## Use 10-fold CV to compute the PMSE and visualize with a boxplot
 K <- 10
 nrow(mod.mat)/K
@@ -389,7 +439,7 @@ for(k in 1:K){
   fit.hs <- sampling(mod, data = standat, iter = 8000) 
   
   # Option 2: Approximate implementation in shrinkem
-
+  
   # get maximum likelihood estimates
   lmfit <- lm(train$ViolentCrimesPerPop ~ -1 + ., train)
   
@@ -489,13 +539,13 @@ save(out, file ="./results/CV_PMSE_crime.RData")
 
 pmse <- do.call(rbind.data.frame, out)
 pmse$Method <- plyr::revalue(pmse$Method, 
-                       c("hs_exact" = "Exact horseshoe",
-                         "hs_shrinkem" = "App. horseshoe",
-                         "lasso_exact" = "Exact lasso ",
-                         "lasso_shrinkem" = "App. lasso",
-                         "ridge_exact" = "Exact ridge",
-                         "ridge_shrinkem" = "App. ridge",
-                         "lm" = "Unregularized"))
+                             c("hs_exact" = "Exact horseshoe",
+                               "hs_shrinkem" = "App. horseshoe",
+                               "lasso_exact" = "Exact lasso ",
+                               "lasso_shrinkem" = "App. lasso",
+                               "ridge_exact" = "Exact ridge",
+                               "ridge_shrinkem" = "App. ridge",
+                               "lm" = "Unregularized"))
 
 png(file = "./results/CV_PMSE_crime.png", width = 1000, height = 800)
 ggplot(pmse, aes(x = Method, y = PMSE)) +
@@ -504,32 +554,179 @@ ggplot(pmse, aes(x = Method, y = PMSE)) +
   theme_bw(base_size = 25)
 dev.off()
 
-## Visualize the results on a diagonal (in line with other figures)
-load("./results/full_results_crime.RData")
+## 10-fold CV PMSE for half of the data set
+N <- round(0.5*nrow(mod.mat))
+sel <- sample(1:nrow(mod.mat), N, replace = FALSE)
+half_df <- mod.mat[sel, ]
 
-approx <- res[which(res$Algorithm == "shrinkem"), ]
-colnames(approx) <- c("Variable", "Prior", "Algorithm", 
-                      "Approx. mean", "Approx. LB", "Approx. UB",
-                      "Approx. mode", "Approx. incl.")
-exact <- res[which(res$Algorithm == "exact"), ]
-colnames(exact) <- c("Variable", "Prior", "Algorithm",
-                     "Exact mean", "Exact LB", "Exact UB",
-                     "Exact mode", "Exact incl.")
+## Use 10-fold CV to compute the PMSE and visualize with a boxplot
+K <- 10
+nrow(half_df)/K
 
-plotdat <- merge(approx, exact, by=c("Variable", "Prior"))
-plotdat$Prior <- plyr::revalue(plotdat$Prior, 
-                             c("hs" = "Horseshoe",
-                               "lasso" = "Lasso",
-                               "ridge" = "Ridge"))
+# scaling the data per fold introduces NAs due to small variability in certain folds (specifically fold 5)
+half_df <- data.frame(scale(half_df[sample(nrow(half_df), replace = FALSE), ])) # shuffle and scale data
+fold <- list()
+fold[[1]] <- half_df[1:16, ]
+fold[[2]] <- half_df[17:(2*16), ]
+fold[[3]] <- half_df[(2*16+1):(3*16), ]
+fold[[4]] <- half_df[(3*16+1):(4*16), ]
+fold[[5]] <- half_df[(4*16+1):(5*16), ]
+fold[[6]] <- half_df[(5*16+1):(6*16), ]
+fold[[7]] <- half_df[(6*16+1):(7*16), ]
+fold[[8]] <- half_df[(7*16+1):(8*16), ]
+fold[[9]] <- half_df[(8*16+1):(9*16), ]
+fold[[10]] <- half_df[(9*16+1):nrow(half_df), ]
 
-png(file = "./results/points_intervals_diagonal_crime.png", width = 1400, height = 800)
-ggplot(plotdat, aes(x = `Exact mean`, y = `Approx. mean`)) +
-  facet_wrap("Prior") +
-  geom_point() +
-  coord_cartesian(xlim = c(-1, 1), ylim = c(-1, 1)) +
-  geom_abline(intercept = 0, slope = 1, lty = 2) +
-  geom_linerange(aes(ymin = `Approx. LB`, ymax = `Approx. UB`)) +
-  geom_linerange(aes(xmin = `Exact LB`, xmax = `Exact UB`)) +
-  xlab("Exact") + ylab("Approximate") +
+# Run the analysis on k-1 folds using the remaining fold as test set
+out <- vector(mode = "list", length = K)
+for(k in 1:K){
+  test <- fold[[k]]
+  fold_train <- fold
+  fold_train[[k]] <- NULL
+  train <- do.call(rbind.data.frame, fold_train)
+  
+  # Option 1: Exact with Stan
+  input.dat <- list(N_train = nrow(train),
+                    p = ncol(train)-1,
+                    y_train = train$ViolentCrimesPerPop,
+                    X_train = train[, -c(grep("ViolentCrimesPerPop", colnames(train)))])
+  
+  # prior hyperparameters
+  s0 = 1
+  nu0 = 3
+  
+  # ridge exact
+  standat <- c(input.dat, 
+               list(s0 = s0,
+                    nu0 = nu0))
+  mod <- stan_model("./models/exact_regression_ridge.stan")
+  fit.ridge <- sampling(mod, data = standat)
+  
+  # lasso exact
+  standat <- c(input.dat, 
+               list(s0 = s0,
+                    nu0 = nu0))
+  mod <- stan_model("./models/exact_regression_lasso.stan")
+  fit.lasso <- sampling(mod, data = standat)
+  
+  # horseshoe exact  
+  standat <- c(input.dat, 
+               list(s0 = s0))
+  mod <- stan_model("./models/exact_regression_hs.stan")
+  fit.hs <- sampling(mod, data = standat, iter = 8000) 
+  
+  # Option 2: Approximate implementation in shrinkem
+  
+  # get maximum likelihood estimates
+  lmfit <- lm(train$ViolentCrimesPerPop ~ -1 + ., train)
+  
+  # extract MLEs
+  mle <- coef(lmfit)
+  covmat <- vcov(lmfit)
+  
+  # ridge approximation shrinkem
+  shrink.ridge <- shrinkem(mle, Sigma = covmat, type = "ridge", iterations = 5000)
+  
+  # lasso approximation shrinkem
+  shrink.lasso <- shrinkem(mle, Sigma = covmat, type = "lasso", iterations = 5000)
+  
+  # horseshoe approximation shrinkem
+  shrink.hs <- shrinkem(mle, Sigma = covmat, type = "horseshoe", iterations = 5000) 
+  
+  # Combine results exact algorithm
+  get.results <- function(fitobj, prior, algorithm, nms = names(mle)){
+    summ <- summary(fitobj, prob = c(0.025, 0.975))$summary
+    if(algorithm == "approx"){
+      outsel <- summ[grep("theta\\[", rownames(summ)), c("mean", "2.5%", "97.5%")]
+    } else if(algorithm == "exact"){
+      outsel <- summ[grep("beta\\[", rownames(summ)), c("mean", "2.5%", "97.5%")]
+    }
+    
+    res <- cbind.data.frame(nms, prior, algorithm, outsel)
+    return(res)
+  }
+  
+  res.ridge1 <- get.results(fitobj = fit.ridge, prior = "ridge", algorithm = "exact")
+  res.lasso1 <- get.results(fitobj = fit.lasso, prior = "lasso", algorithm = "exact")
+  res.hs1 <- get.results(fitobj = fit.hs, prior = "hs", algorithm = "exact")
+  
+  res.exact <- rbind.data.frame(res.ridge1, res.lasso1, res.hs1)
+  
+  ## add results shrinkem
+  res.ridge <- summary(shrink.ridge)
+  res.ridge <- res.ridge[, c(grep("shrunk.mean|shrunk.lower|shrunk.upper", colnames(res.ridge)))]
+  colnames(res.ridge) <- c("Mean", "LB", "UB")
+  res.ridge$Variable <- rownames(res.ridge)
+  res.ridge$Prior <- "ridge"
+  res.ridge$Algorithm <- "shrinkem"
+  
+  res.lasso <- summary(shrink.lasso)
+  res.lasso <- res.lasso[, c(grep("shrunk.mean|shrunk.lower|shrunk.upper", colnames(res.lasso)))]
+  colnames(res.lasso) <- c("Mean", "LB", "UB")
+  res.lasso$Variable <- rownames(res.lasso)
+  res.lasso$Prior <- "lasso"
+  res.lasso$Algorithm <- "shrinkem"
+  
+  res.hs <- summary(shrink.hs)
+  res.hs <- res.hs[, c(grep("shrunk.mean|shrunk.lower|shrunk.upper", colnames(res.hs)))]
+  colnames(res.hs) <- c("Mean", "LB", "UB")
+  res.hs$Variable <- rownames(res.hs)
+  res.hs$Prior <- "hs"
+  res.hs$Algorithm <- "shrinkem"
+  
+  res.shrink <- rbind.data.frame(res.ridge, res.lasso, res.hs)
+  colnames(res.exact) <- c("Variable", "Prior", "Algorithm", "Mean", "LB", "UB")
+  res <- rbind.data.frame(res.exact, res.shrink)
+  
+  # Compute PMSE
+  testX <- as.data.frame(t(test[, -grep("ViolentCrimesPerPop", colnames(test))]))
+  testX$Variable <- rownames(testX)
+  testY <- test$ViolentCrimesPerPop
+  
+  res$Method <- factor(paste(res$Prior, res$Algorithm, sep = "_"))
+  out[[k]] <- data.frame(NA)
+  for(i in 1:length(levels(res$Method))){
+    sel <- res[which(res$Method == levels(res$Method)[i]), c("Variable", "Mean")]
+    comb <- merge(sel, testX, by = "Variable")
+    
+    test.obs <- comb[, -c(grep("Variable|Mean", colnames(comb)))]
+    est <- comb$Mean
+    predY <- apply(test.obs, 2, function(x) sum(est*x))
+    pmse <- mean((testY - predY)^2)
+    
+    out[[k]][i, 1] <- levels(res$Method)[i]
+    out[[k]][i, 2] <- pmse
+  }
+  
+  colnames(out[[k]]) <- c("Method", "PMSE")
+  
+  # add MSE regular lm
+  sel <- data.frame("Estimate" = lmfit$coefficients,
+                    "Variable" = names(lmfit$coefficients))
+  comb <- merge(sel, testX, by = "Variable")
+  test.obs <- comb[, -c(grep("Variable|Estimate", colnames(comb)))]
+  est <- comb$Estimate
+  predY <- apply(test.obs, 2, function(x) sum(est*x))
+  pmse <- mean((testY - predY)^2)
+  out[[k]][7, 1] <- "lm"
+  out[[k]][7, 2] <- pmse
+}
+
+save(out, file ="./results/CV_PMSE_crime_half.RData")
+
+pmse <- do.call(rbind.data.frame, out)
+pmse$Method <- plyr::revalue(pmse$Method, 
+                             c("hs_exact" = "Exact horseshoe",
+                               "hs_shrinkem" = "App. horseshoe",
+                               "lasso_exact" = "Exact lasso ",
+                               "lasso_shrinkem" = "App. lasso",
+                               "ridge_exact" = "Exact ridge",
+                               "ridge_shrinkem" = "App. ridge",
+                               "lm" = "Unregularized"))
+
+png(file = "./results/CV_PMSE_crime_half.png", width = 1000, height = 800)
+ggplot(pmse, aes(x = Method, y = PMSE)) +
+  geom_boxplot() +
+  scale_x_discrete(guide = guide_axis(angle = 90)) +
   theme_bw(base_size = 25)
 dev.off()
